b1dd472bab2db0f3c5f6aa3daac21da4ff178218
aiotechhub <aiotechhub@gmail.com>
2023-06-28 19:44:24 +0530

    updated uri

--- src/main/java/com/aiotech/controller/DemoController.java
+++ src/main/java/com/aiotech/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
-    @GetMapping("message")
+    @GetMapping("/message")
     public String message(){
         return "Congrats! You have successfully deployed your application in Azure Cloud!";
     }
