9d970338cfed0486a6f49be9245a858e8fc33926
aiotechhub <aiotechhub@gmail.com>
2023-06-28 19:14:28 +0530

    added demo controller for testing purpose

---
+++ src/main/java/com/aiotech/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.aiotech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("message")
+    public String message(){
+        return "Congrats! You have successfully deployed your application in Azure Cloud!";
+    }
+}
